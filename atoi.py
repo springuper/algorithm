d4c0e94d5d5155f489e0c1f04759c4344fc1c38d
shangchun <shangchun@meituan.com>
2014-11-27 10:04:26 +0800

    format

--- atoi.py
+++ atoi.py
@@ -1,28 +1,28 @@
 # @return an integer
 def atoi(s):
-        n = 0
-        sign = 1
-        prev = ''
-        for i in s:
-            if prev == '' or prev == ' ':
-                if i == '+':
-                    sign = 1
-                elif i == '-':
-                    sign = -1
-                elif i >= '0' and i <= '9':
-                    n = n * 10 + ord(i) - ord('0')
-                elif i != ' ':
-                    return 0
-            elif prev == '+' or prev == '-' or (prev >= '0' and prev <= '9'):
-                if i < '0' or i > '9':
-                    break;
+    n = 0
+    sign = 1
+    prev = ''
+    for i in s:
+        if prev == '' or prev == ' ':
+            if i == '+':
+                sign = 1
+            elif i == '-':
+                sign = -1
+            elif i >= '0' and i <= '9':
                 n = n * 10 + ord(i) - ord('0')
-                if n * sign > 2147483647:
-                    return 2147483647
-                elif n * sign < -2147483648:
-                    return -2147483648
-            prev = i
-        return n * sign
+            elif i != ' ':
+                return 0
+        elif prev == '+' or prev == '-' or (prev >= '0' and prev <= '9'):
+            if i < '0' or i > '9':
+                break;
+            n = n * 10 + ord(i) - ord('0')
+            if n * sign > 2147483647:
+                return 2147483647
+            elif n * sign < -2147483648:
+                return -2147483648
+        prev = i
+    return n * sign
 
 
 assert atoi('123') == 123
